1e4f8281eb357a330e6a15da6c3fa795da9be7f5
Parveen Chahal <mail.chahal@gmail.com>
2024-12-03 16:45:30 -0800

    Update pbash-args.sh

--- pbash-args.sh
+++ pbash-args.sh
@@ -268,7 +268,7 @@ function pbash.args.__updates.need_update {
   return 0
 }
 
-pbash.args.__updates.need_update || echo "WARNING: pbash-args.sh has a version available. Run 'wget -q -O - https://pbash.pcapis.com/args/install.sh | sudo bash'"
+pbash.args.__updates.need_update || echo "WARNING: pbash-args.sh has a version available. Run either 'pbash.args.update_latest_version' or 'wget -q -O - https://pbash.pcapis.com/args/install.sh | sudo bash'"
 
 function pbash.args.update_latest_version() {
   wget -q -O - https://pbash.pcapis.com/args/install.sh | sudo bash
