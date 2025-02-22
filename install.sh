95cfa2ca0c556927acb618814484e29ca46ead4c
Parveen Chahal <mail.chahal@gmail.com>
2025-02-22 01:38:19 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl -o -L /usr/local/bin/pbash-args.sh https://pbash.pcapis.com/args/pbash-args.sh
+curl -L -o /usr/local/bin/pbash-args.sh https://pbash.pcapis.com/args/pbash-args.sh
 if [ "$?" == "0" ]
 then
   chmod +x /usr/local/bin/pbash-args.sh
