1fc7b371416075e252032700e3aa76d516b20256
Parveen Chahal <mail.chahal@gmail.com>
2025-03-05 02:42:25 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 installation_type=$1
 installation_path=""
 [ "$installation_type" == "--system" ] && installation_path="/usr/local/bin/pbash-args.sh"
-[ "$installation_type" == "--user" ] && installation_path="$HOME/.local/bin/pbash-args.sh"
+[ "$installation_type" == "--user" ] && installation_path="$HOME/.local/bin/pbash-args.sh" && $([ -d .local/bin ] || mkdir -p .local/bin)
 if [ -z "$installation_path" ]
 then
   echo "Installation type is not provided. Either --user or --system argument is required."
