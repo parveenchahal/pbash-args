85ab826f3729c7b5c265e086fb90e31d0f2a2480
Parveen Chahal <mail.chahal@gmail.com>
2025-03-05 02:26:48 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+installation_type=$1
+installation_path=""
+[ "$installation_type" == "--system" ] && installation_path="/usr/local/bin/pbash-args.sh"
+[ "$installation_type" == "--user" ] && installation_path="$HOME/.local/bin/pbash-args.sh"
+if [ -z "$installation_type" ]
+then
+  echo "Installation type is not provided. Either --user or --system argument is required."
+  exit 1
+fi
 curl -sL -o /usr/local/bin/pbash-args.sh https://pbash.pcapis.com/args/pbash-args.sh
 if [ "$?" == "0" ]
 then
